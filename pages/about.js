b8b874df85996c31dd1c99d3b6a5b069d2160c80
PhiX <pokphoenix@gmail.com>
2022-02-02 14:35:58 +0700

    Layout , Header , Footer

--- pages/about.js
+++ pages/about.js
@@ -1,8 +1,16 @@
+import Link from "next/link";
+import Layout from '../components/Layout'
+
+
 export default function AboutPage() {
   return (
     <div>
-      <h1>AboutPage</h1>
-      <p>This is an app to find the lastest DJ and other musical events</p>
+          <Layout title='About DJ Events' >
+            <h1>AboutPage</h1>
+            <p>This is an app to find the lastest DJ and other musical events</p>
+            <Link href='/'>Home</Link>
+          </Layout>
+      
     </div>
   )
 }
