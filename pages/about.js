f2697a1d8e6c10a0f88446606fef977bbb49499e
PhiX <pokphoenix@gmail.com>
2022-02-02 12:23:57 +0700

    page and routing

---
+++ pages/about.js
@@ -0,0 +1,8 @@
+export default function AboutPage() {
+  return (
+    <div>
+      <h1>AboutPage</h1>
+      <p>This is an app to find the lastest DJ and other musical events</p>
+    </div>
+  )
+}
