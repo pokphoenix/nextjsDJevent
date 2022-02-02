8a8daa1d005acf1e8e72d13f0953e45dab7868ea
PhiX <pokphoenix@gmail.com>
2022-02-02 15:10:19 +0700

    Show case component

--- components/Layout.js
+++ components/Layout.js
@@ -2,8 +2,11 @@ import Head from 'next/head'
 import styles from '@/styles/Layout.module.css'
 import Footer from './Footer'
 import Header from './Header'
+import Showcase from './Showcase'
+import { useRouter } from 'next/router'
 
 export default function Layout({title, keywords, description, children}) {
+  const router = useRouter()
   return (
     <div>
         <Head>
@@ -13,10 +16,12 @@ export default function Layout({title, keywords, description, children}) {
             <link rel="icon" href="/favicon.ico" />
         </Head> 
         <Header />
+        {router.pathname ==='/' && <Showcase/>}  
         <div className={styles.container}>
             {children}
         </div>
         <Footer/>
+       
       
 
     </div>
