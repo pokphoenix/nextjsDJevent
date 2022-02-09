c8b592c4d3a4c3bc3de10bebf43e6efdf2168e03
PhiX <pokphoenix@gmail.com>
2022-02-09 14:25:02 +0700

    Modal component

--- pages/events/edit/[id].js
+++ pages/events/edit/[id].js
@@ -1,5 +1,6 @@
 import moment from 'moment'
 import Layout from '@/components/Layout'
+import Modal from '@/components/Modal'
 import Link from 'next/link'
 import { useRouter } from 'next/router'
 import { useState } from 'react'
@@ -23,6 +24,8 @@ export default function EditEventPage({evt}) {
   })
 
   const [imagePreview, setImagePreview] = useState( evt.attributes.image && evt.attributes.image.data ?  evt.attributes.image.data.attributes.formats.thumbnail.url : null)
+
+  const [showModal, setShowModal] = useState(false)
 
   const router = useRouter()
   const handleSubmit = async (e) => {
@@ -66,92 +69,92 @@ export default function EditEventPage({evt}) {
             <h1>Edit Event</h1>
             <ToastContainer />
             <form onSubmit={handleSubmit} className={styles.form}>
-        <div className={styles.grid}>
-          <div>
-            <label htmlFor='name'>Event Name</label>
-            <input
-              type='text'
-              id='name'
-              name='name'
-              value={values.name}
-              onChange={handleInputChange}
-            />
-          </div>
-          <div>
-            <label htmlFor='slug'>Slug</label>
-            <input
-              type='text'
-              id='slug'
-              name='slug'
-              value={values.slug}
-              onChange={handleInputChange}
-            />
-          </div>
-          <div>
-            <label htmlFor='performers'>Performers</label>
-            <input
-              type='text'
-              name='performers'
-              id='performers'
-              value={values.performers}
-              onChange={handleInputChange}
-            />
-          </div>
-          <div>
-            <label htmlFor='venue'>Venue</label>
-            <input
-              type='text'
-              name='venue'
-              id='venue'
-              value={values.venue}
-              onChange={handleInputChange}
-            />
-          </div>
-          <div>
-            <label htmlFor='address'>Address</label>
-            <input
-              type='text'
-              name='address'
-              id='address'
-              value={values.address}
-              onChange={handleInputChange}
-            />
-          </div>
-          <div>
-            <label htmlFor='date'>Date</label>
-            <input
-              type='date'
-              name='date'
-              id='date'
-              value={moment(values.date).format('yyyy-MM-DD')}
-              onChange={handleInputChange}
-            />
-          </div>
-          <div>
-            <label htmlFor='time'>Time</label>
-            <input
-              type='text'
-              name='time'
-              id='time'
-              value={values.time}
-              onChange={handleInputChange}
-            />
-          </div>
-        </div>
+                <div className={styles.grid}>
+                <div>
+                    <label htmlFor='name'>Event Name</label>
+                    <input
+                    type='text'
+                    id='name'
+                    name='name'
+                    value={values.name}
+                    onChange={handleInputChange}
+                    />
+                </div>
+                <div>
+                    <label htmlFor='slug'>Slug</label>
+                    <input
+                    type='text'
+                    id='slug'
+                    name='slug'
+                    value={values.slug}
+                    onChange={handleInputChange}
+                    />
+                </div>
+                <div>
+                    <label htmlFor='performers'>Performers</label>
+                    <input
+                    type='text'
+                    name='performers'
+                    id='performers'
+                    value={values.performers}
+                    onChange={handleInputChange}
+                    />
+                </div>
+                <div>
+                    <label htmlFor='venue'>Venue</label>
+                    <input
+                    type='text'
+                    name='venue'
+                    id='venue'
+                    value={values.venue}
+                    onChange={handleInputChange}
+                    />
+                </div>
+                <div>
+                    <label htmlFor='address'>Address</label>
+                    <input
+                    type='text'
+                    name='address'
+                    id='address'
+                    value={values.address}
+                    onChange={handleInputChange}
+                    />
+                </div>
+                <div>
+                    <label htmlFor='date'>Date</label>
+                    <input
+                    type='date'
+                    name='date'
+                    id='date'
+                    value={moment(values.date).format('yyyy-MM-DD')}
+                    onChange={handleInputChange}
+                    />
+                </div>
+                <div>
+                    <label htmlFor='time'>Time</label>
+                    <input
+                    type='text'
+                    name='time'
+                    id='time'
+                    value={values.time}
+                    onChange={handleInputChange}
+                    />
+                </div>
+                </div>
 
-        <div>
-          <label htmlFor='description'>Event Description</label>
-          <textarea
-            type='text'
-            name='description'
-            id='description'
-            value={values.description}
-            onChange={handleInputChange}
-          ></textarea>
-        </div>
+                <div>
+                <label htmlFor='description'>Event Description</label>
+                <textarea
+                    type='text'
+                    name='description'
+                    id='description'
+                    value={values.description}
+                    onChange={handleInputChange}
+                ></textarea>
+                </div>
 
-        <input type='submit' value='Update Event' className='btn' />
-      </form>
+                <input type='submit' value='Update Event' className='btn' />
+            </form>
 
             <h2>Event Image</h2>
             {imagePreview ? (
@@ -159,11 +162,14 @@ export default function EditEventPage({evt}) {
             ) : <div> <p>No image uploaded</p> </div> }
 
             <div>
-                <button className='btn-secondary'>
+                <button onClick={() => setShowModal(true)} className='btn-secondary'>
                     <FaImage /> Set Image
                 </button>
             </div>
 
+            <Modal show={showModal} onClose={() => setShowModal(false)}>
+                IMAGE UPLOAD
+            </Modal>
         </Layout>
   )
 }
